1e10697a24fda8cdf5a05047a6973153448498f7
Андрей Миронов <avale1648@gmail.com>
2024-02-16 01:40:21 +0300

    Update Homework 2023_01_31

--- Homeworks/Homework_2023_01_31/src/console/CommandManager.java
+++ Homeworks/Homework_2023_01_31/src/console/CommandManager.java
@@ -14,54 +14,57 @@ public class CommandManager {
     }
     public void execute() {
         System.out.println("type /help to see commands.");
-        while(true) {
-            input = scanner.nextLine();
-            var split = input.split(" ");
-            if(split[0].equals("/checkIsLeap")) {
-                var year = randomOrNumber(split[1]);
-                var leapYearChecker = new LeapYearChecker(year);
-                System.out.println(leapYearChecker.toString());
+        try {
+            while (true) {
+                input = scanner.nextLine();
+                var split = input.split(" ");
+                if (split[0].equals("/checkIsLeap")) {
+                    var year = randomOrNumber(split[1]);
+                    var leapYearChecker = new LeapYearChecker(year);
+                    System.out.println(leapYearChecker.toString());
+                }
+                if (split[0].equals("/swapBytes")) {
+                    var number = randomOrNumber(split[1]);
+                    var byteSwapper = new ByteSwapper(number);
+                    System.out.println(byteSwapper.toString());
+                }
+                if (split[0].equals("/collatzCount")) {
+                    var number = randomOrNumber(split[1]);
+                    var collatzCounter = new CollatzCounter(number);
+                    collatzCounter.count();
+                }
+                if (split[0].equals("/zeroOneCount")) {
+                    var length = randomOrNumber(split[1]);
+                    var zeroOneCounter = new ZeroOneCounter(length);
+                    zeroOneCounter.count();
+                    System.out.println(zeroOneCounter.toString());
+                }
+                if (split[0].equals("/sortArray")) {
+                    var length = randomOrNumber(split[1]);
+                    var arraySorter = new ArraySorter(length);
+                    arraySorter.fill();
+                    arraySorter.gnomeSort();
+                }
+                if (input.equals("/help")) {
+                    System.out.println(
+                            "/checkIsLeap [year] - check is [year] leap or not;\n" +
+                                    "/swapBytes [number] - swap two lower and upper bytes in [number];\n" +
+                                    "/collatzCount [number] - calculate Collatz Conjecture for [number];\n" +
+                                    "/zeroOneCount [length] - count zeros and ones in array of [length];\n" +
+                                    "/sortArray [length] - sort array with length [length];\n" +
+                                    "/stop - stops execution.\n\n" +
+                                    "[year],[number],[length] - integer parameter, use \"random\" to generate a random number."
+                    );
+                }
+                if (input.equals("/stop")) {
+                    break;
+                }
             }
-            if(split[0].equals("/swapBytes")) {
-                var number = randomOrNumber(split[1]);
-                var byteSwapper = new ByteSwapper(number);
-                System.out.println(byteSwapper.toString());
-            }
-            if(split[0].equals("/collatzCount")) {
-                var number = randomOrNumber(split[1]);
-                var collatzCounter = new CollatzCounter(number);
-                collatzCounter.count();
-            }
-            if(split[0].equals("/zeroOneCount")){
-                var length = randomOrNumber(split[1]);
-                var zeroOneCounter = new ZeroOneCounter(length);
-                zeroOneCounter.count();
-                System.out.println(zeroOneCounter.toString());
-            }
-            if(split[0].equals("/sortArray")){
-                var length = randomOrNumber(split[1]);
-                var arraySorter = new ArraySorter(length);
-                arraySorter.fill();
-                arraySorter.gnomeSort();
-            }
-            if(input.equals("/help")){
-                System.out.println(
-                        "/checkIsLeap [year] - check is [year] leap or not;\n" +
-                                "/swapBytes [number] - swap two lower and upper bytes in [number];\n" +
-                                "/collatzCount [number] - calculate Collatz Conjecture for [number];\n" +
-                                "/zeroOneCount [length] - count zeros and ones in array of [length];\n" +
-                                "/sortArray [length] - sort array with length [length];\n" +
-                                "/stop - stops execution.\n\n" +
-                                "[year],[number],[length] - integer parameter, use \"random\" to generate a random number."
-                );
-            }
-            if(input.equals("/stop")) {
-                break;
-            }
+        } catch (Exception e){
+            System.out.println(e.toString());
         }
     }
-    private int randomOrNumber(String str)
-    {
+    private int randomOrNumber(String str) {
         var result = 0;
         if(str.equals("random")) {
             var random = new Random();
--- Homeworks/Homework_2023_01_31/src/util/ArraySorter.java
+++ Homeworks/Homework_2023_01_31/src/util/ArraySorter.java
@@ -2,48 +2,38 @@ package util;
 import java.sql.Array;
 import java.util.*;
 public class ArraySorter {
-    private int[] numbers;
+    private int length;
+    private ArrayList<Integer> numbers;
     public ArraySorter(int length) {
-        numbers = new int[length];
+        this.length = length;
+        numbers = new ArrayList<Integer>();
     }
     public void fill() {
-        int i, j;
-        var random = new Random();
-        for(i = 0; i < numbers.length;) {
-            int randomNumber = random.nextInt(numbers.length) + 1;
-            for(j = 0; j < numbers.length; j++){
-                if(numbers[j] == randomNumber)
-                    break;
-           }
-           if(i == j) {
-               numbers[i] = randomNumber;
-               i++;
+       var random = new Random();
+        while(numbers.size() < length) {
+           var a = random.nextInt(length) + 1;
+           if(!numbers.contains(a)){
+               numbers.add(a);
            }
        }
-        System.out.println("Unsorted Array:" + toString());
+        System.out.println("Unsorted array:" + numbers);
     }
     public void gnomeSort() {
         var index = 0;
-        while(index < numbers.length){
-            if(index == 0){
+        while(index < numbers.size()) {
+            if (index == 0) {
                 index++;
             }
-            if(numbers[index] >= numbers[index - 1]) {
+            if (numbers.get(index) >= numbers.get(index - 1)) {
                 index++;
-            }
-            else{
+            } else {
                 int temp = 0;
-                temp = numbers[index];
-                numbers[index] = numbers[index - 1];
-                numbers[index - 1] = temp;
+                temp = numbers.get(index);
+                numbers.set(index, numbers.get(index - 1));
+                numbers.set(index - 1, temp);
                 index--;
             }
         }
-        System.out.println("Sorted Array:" + toString());
-    }
-    @Override
-    public String toString()
-    {
-        return Arrays.toString(numbers);
+        System.out.println("Sorted array:" + numbers);
     }
 }
